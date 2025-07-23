31fe4ecbda37b43c42acba874741de8c93b8a055
lepcoutinho <lepcoutinho@gmail.com>
2025-07-23 20:07:35 -0300

    Comp.: User Ajustado

--- components/user/user.tsx
+++ components/user/user.tsx
@@ -1,58 +1,59 @@
 // 'use client';
 
-import style from './style.module.scss'
-import Image from 'next/image'
+import style from "./style.module.scss";
+import Image from "next/image";
 
-import Avatar from '../../Img/Avatar.png'
+import Avatar from "../../Img/Avatar.png";
 
 import {
-    GrUser,
-    GrNotification,
-    GrMoreVertical,
-    GrClose,
-    GrLogout,
-    GrConfigure,
-    GrPieChart
+  GrUser,
+  GrNotification,
+  GrMoreVertical,
+  GrClose,
+  GrLogout,
+  // GrConfigure,
+  // GrPieChart
 } from "react-icons/gr";
 
 const User = () => {
-    return (
-        <section className={style.User}>
+  return (
+    <section className={style.User}>
+      <article>
+        <div className={style.Img}>
+          <div className={style.Notifi}>
+            <GrNotification />
+          </div>
+          <Image src={Avatar} alt={"Logomarca"} />
+        </div>
 
-            <article>
-                <div className={style.Img}>
-                    <div className={style.Notifi}><GrNotification /></div>
-                    <Image src={Avatar}
-                        alt={'Logomarca'}
-                    />
-                </div>
+        <input type="checkbox" id="menu" className={style.checkbox} />
+        <label htmlFor="menu">
+          <GrMoreVertical className={style.IconOpen} />
+          <GrClose className={style.IconClose} />
+        </label>
 
+        <ul className={style.listMenu}>
+          <Image src={Avatar} alt={"Logomarca"} />
+          <p>Joaquim Silva</p>
+          <li>
+            <a>
+              {" "}
+              <GrUser />
+              PERFIL
+            </a>
+          </li>
+          <li>
+            <a>
+              {" "}
+              <GrLogout />
+              SAIR
+            </a>
+          </li>
+        </ul>
+      </article>
 
-                <input type="checkbox" id="menu" className={style.checkbox} />
-                <label htmlFor="menu">
-                    <GrMoreVertical className={style.IconOpen} />
-                    <GrClose className={style.IconClose} />
-                </label>
+    </section>
+  );
+};
 
-                <ul className={style.listMenu}>
-                <Image src={Avatar}
-                        alt={'Logomarca'}
-                    />
-                    <p>Joaquim Silva</p>
-                    <li><a> <GrUser />PERFIL</a></li>
-                    <li><a> <GrLogout />SAIR</a></li>
-                </ul>
-
-            </article>
-
-            <div>
-
-            </div>
-
-
-
-        </section>
-    )
-}
-
-export default User
+export default User;
